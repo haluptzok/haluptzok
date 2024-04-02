63dfc6647d1a16928ebfad0a4deb0bc5d083f416
Patrick Haluptzok <haluptzok@live.com>
2024-04-02 13:45:39 -0700

    do some tests

--- tst.bat
+++ tst.bat
@@ -1,1 +1,5 @@
-python makemore.py --gentext=1 --block_size=32 --learning-rate=0.001
+python makemore.py --device=cpu --gentext=1 --block_size=32 --learning-rate=0.001
+python makemore.py --device=cuda --gentext=1 --block_size=32 --learning-rate=0.001
+python makemore.py --device=cuda --gentext=1 --block_size=32 --learning-rate=0.001 --max-steps=10000
+python makemore.py --device=cpu --gentext=1 --block_size=32 --learning-rate=0.001 --max-steps=10000
+
